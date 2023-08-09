c16447d92a0e74ca0096aa3c1b7556622f88c382
John Aribatise <johnaribatise@hsass.org>
2023-08-09 13:50:14 +0100

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import Header from "./NFTCOMPONENTS/Header/Header";
 import img from './Images/nft9.jpg'
-import Explore from "./Explore/Explore";
-import Discover from "./NFTCOMPONENTS/Discover/Discover";
 import image from './Images/NFT3.jpg'
 import newsImage from './Images/news1.jpg'
-import News from "./NFTCOMPONENTS/Newsletter/News";
+import { lazy, Suspense , useState, useEffect } from "react";
+
+const Header = lazy(() => import("./NFTCOMPONENTS/Header/Header"))
+const Explore = lazy(() => import("./Explore/Explore"))
+const Discover = lazy(() => import("./NFTCOMPONENTS/Discover/Discover"))
+const News = lazy(() => import("./NFTCOMPONENTS/Newsletter/News"))
 
 function App() {
+  const [showHeader, setShowHeader] = useState(false);
+  const [showExplore, setShowExplore] = useState(false);
+  const [showDiscover, setShowDiscover] = useState(false);
+  const [showNews, setShowNews] = useState(false);
+
+   useEffect(() => {
+    const headerTimeout = setTimeout(() => setShowHeader(true), 3000);
+    const exploreTimeout = setTimeout(() => setShowExplore(true), 3000);
+    const discoverTimeout = setTimeout(() => setShowDiscover(true), 9000);
+    const newsTimeout = setTimeout(() => setShowNews(true), 12000);
+
+    // Clear timeouts when component unmounts
+    return () => {
+      clearTimeout(headerTimeout);
+      clearTimeout(exploreTimeout);
+      clearTimeout(discoverTimeout);
+      clearTimeout(newsTimeout);
+    };
+  }, []);
+
   return (
     <div>
-      <Header img={img}/>
-      <Explore/>
-      <Discover img={image}/>
-      <News img={newsImage}/>
+      <Suspense>
+        {showHeader &&<Header img={img}/>}
+      </Suspense>
+      <Suspense>
+        {showExplore && <Explore />}
+      </Suspense>
+      <Suspense>
+        {showDiscover && <Discover img={image} />}
+      </Suspense>
+      <Suspense>
+        {showNews && <News img={newsImage} />}
+      </Suspense>
     </div>
   );
 }
